e03803e15e035203dbc2718f0baeeed3b93e84d0
Frederik Gheysels <frederik.gheysels@codit.eu>
2023-02-03 21:20:47 +0100

    Initial commit

---
+++ src/Fg.SolarProductionAlerter/Fg.SolarProductionAlerter/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Fg.SolarProductionAlerter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
